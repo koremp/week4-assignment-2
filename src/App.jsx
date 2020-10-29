0cc449285004879f6dc93393d2de4bedba95509c
Dokyun Lim <im.koremp@gmail.com>
2020-10-29 19:16:20 +0900

    App 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import {useDispatch} from 'react-redux';
+
+function RestaurantsContainer() {
+  return null;
+}
+
+function RestaurantInputContainer() {
+  return null;
+}
+
 export default function App() {
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <RestaurantsContainer />
+      <RestaurantInputContainer/>
     </div>
   );
 }
