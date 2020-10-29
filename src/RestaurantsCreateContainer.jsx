2db5ed5b6c59f27c93ce1ff30e226bbed2cd5a96
Dokyun Lim <im.koremp@gmail.com>
2020-10-29 20:44:09 +0900

    RestaurantsCreateContainer 구현

---
+++ src/RestaurantsCreateContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function RestaurantsCreateContainer() {
+  return (
+    <div>
+      <button type="button">
+        등록
+      </button>
+    </div>
+  );
+}
