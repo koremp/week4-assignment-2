a8e232a14258c41c2f732e2a7310f8418115cb4a
Dokyun Lim <im.koremp@gmail.com>
2020-10-30 00:02:01 +0900

    App test 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,24 +4,20 @@ import { render } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
 import App from './App';
 
-import restaurants from '../fixtures/restaurants';
-
 test('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
-    restaurants,
-    restaurant: [],
+    restaurants: [],
+    restaurant: {},
   }));
 
   const { getByText } = render(<App />);
 
   expect(getByText('Restaurants')).not.toBeNull();
-  expect(dispatch).toBeCalledWith(
-    {
-      type: 'setRestaurants',
-      payload: { restaurants },
-    },
-  );
+  expect(dispatch).toBeCalledWith({
+    type: 'setRestaurants',
+    payload: { restaurants: [] },
+  });
 });
