81f82e7e4ef6e925339c3622db1d82b041d08a1c
Dokyun Lim <im.koremp@gmail.com>
2020-10-29 20:32:23 +0900

    App 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
+
+import {
+  setRestaurants,
+} from './actions';
+
+import restaurants from '../fixtures/restaurants';
 
 function RestaurantsContainer() {
   return null;
@@ -11,11 +17,17 @@ function RestaurantInputContainer() {
 }
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRestaurants(restaurants));
+  }, []);
+
   return (
     <div>
       <h1>Restaurants</h1>
       <RestaurantsContainer />
-      <RestaurantInputContainer/>
+      <RestaurantInputContainer />
     </div>
   );
 }
